a1aa385dc285b1d71cb97d3d74c3331e5dc473f2
Dan Dimond <dtdimond@gmail.com>
2015-09-16 15:22:09 -0400

    Added users and sessions controller and templates for creating new users and logging in, as well as exposing their routes. Haven't connected the routes to the main page via links yet though.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :contacts
   has_many :communications
 
+  has_secure_password validations: false, unique: true
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 4}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ PostitTemplate::Application.routes.draw do
   root to: 'contacts#index'
 
   resources :contacts, except: [:destroy]
+
+  #Session routes
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  #Users
+  get '/register', to: 'users#new'
+  resources :users, only: [:create, :show, :edit, :update]
 end
